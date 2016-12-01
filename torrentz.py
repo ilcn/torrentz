54d6488a42eafe7561d8651a7c66b8acf5e55f5b
user <zd@Zidis-MacBook-Pro.local>
2016-11-30 21:36:50 -0600

    minor updates

--- torrentz.py
+++ torrentz.py
@@ -15,16 +15,38 @@ class Result:
         self.leechers = {}
         self.retrieved_times = {}
         self.magnet_links = {}
+        self.trackers = {}
 
     def print_all(self):
         for x in self.titles.keys():
-            print(self.titles[x], self.creations_times[x], self.sizes[
-                  x], self.peers[x], self.leechers[x], self.retrieved_times[x], self.magnet_links[x])
+            print(self.titles[x], self.creations_times[x], self.sizes[x], self.peers[
+                  x], self.leechers[x], self.retrieved_times[x], self.magnet_links[x], self.trackers[x])
+
+    def get(self, infohash):
+        try:
+            oneresult = {"title": self.titles[infohash]}
+            oneresult["creation_time"] = self.creations_times[infohash]
+            oneresult["size"] = self.sizes[infohash]
+            oneresult["peers"] = self.peers[infohash]
+            oneresult["leechers"] = self.leechers[infohash]
+            oneresult["retrieved_time"] = self.retrieved_times[infohash]
+            oneresult["magnet_link"] = self.magnet_links[infohash]
+            oneresult["infohash"] = infohash
+            oneresult["trackers"] = self.trackers[infohash]
+
+            for x in oneresult.values():
+                if x is None:
+                    return None
+        except Exception as e:
+            oneresult = None
+            print("result set to None due to the following exception")
+            raise e
+        return oneresult
 
 
 def add_trackers(result):
     """
-    takes in result with no tracker information, 
+    takes in result with no tracker information,
     returns one with link with trackers
     """
     for infohash in result.titles.keys():
@@ -37,6 +59,7 @@ def add_trackers(result):
             if link[0:3] == 'udp' or link[0:4] == 'http' and link[len(link) - 8:len(link)] == "announce":
                 trackers.append(link)
         # print(trackers)
+        result.trackers[infohash] = trackers
         dn = urlencode(dict(dn=result.titles[infohash]))
         trs = '&'.join(map(lambda t: urlencode(dict(tr=t)), trackers))
         uri = 'magnet:?xt=urn:btih:%s&%s&%s' % (infohash, dn, trs)
@@ -75,4 +98,5 @@ def search(term):
 
 
 if __name__ == "__main__":
-    search("star")
+    result = search("star")
+    # result.print_all()
